1553c12e2abf6e2be5d6a21e249861c9be893054
Azraf <azraf.almas@gmail.com>
2025-05-04 02:06:04 -0400

    Write all the parse data stuff in this branch onwards

---
+++ ParseSite.java
@@ -0,0 +1,11 @@
+package CallWrapper;
+
+
+// HTML/ CSS Scraping Libraries (JSOUP)
+
+public class ParseSite{
+
+    public static void main(String args[]){
+        System.out.println("Java Test");
+    }
+}
